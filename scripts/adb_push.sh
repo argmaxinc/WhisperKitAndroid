198e2966457232f26eb37faf874124aff88826d9
Keith Ha <keith@argmaxinc.com>
2024-12-05 15:29:03 -0800

    renamed/relocated models

--- scripts/adb_push.sh
+++ scripts/adb_push.sh
@@ -5,14 +5,19 @@
 CURRENT_DIR="$(dirname "$(realpath "$0")")"
 SOURCE_DIR="$CURRENT_DIR/.."
 
-AXIE_TFLITE_CLI="$SOURCE_DIR/build/axie_tflite"
-LOCAL_LIBS="$SOURCE_DIR/libs"
-LOCAL_MODELS="$SOURCE_DIR/models"
+AXIE_TFLITE_CLI="$SOURCE_DIR/build_android/whisperax_cli"
+AXIE_TFLITE_LIB="$SOURCE_DIR/build_android/libwhisperax.so"
+LOCAL_LIBS="$SOURCE_DIR/libs/android"
+LOCAL_TINY_DIR="$SOURCE_DIR/openai_whisper-tiny"
+LOCAL_BASE_DIR="$SOURCE_DIR/openai_whisper-base"
+LOCAL_SMALL_DIR="$SOURCE_DIR/openai_whisper-small"
 LOCAL_INPUTS="$SOURCE_DIR/inputs"
 
 REMOTE_BIN_DIR="/data/local/tmp/bin"
 REMOTE_LIB_DIR="/data/local/tmp/lib"
-REMOTE_MODELS_DIR="/sdcard/argmax/tflite/models"
+REMOTE_TINY_DIR="/sdcard/argmax/tflite/openai_whisper-tiny"
+REMOTE_BASE_DIR="/sdcard/argmax/tflite/openai_whisper-base"
+REMOTE_SMALL_DIR="/sdcard/argmax/tflite/openai_whisper-small"
 REMOTE_INPUTS_DIR="/sdcard/argmax/tflite/inputs"
 
 # Function to push files only if they do not exist
@@ -41,6 +46,9 @@ push_if_not_exists() {
 
 # Push the files and folders to the Android device
 adb push "$AXIE_TFLITE_CLI" "$REMOTE_BIN_DIR/"
+adb push "$AXIE_TFLITE_LIB" "$REMOTE_LIB_DIR/"
 push_if_not_exists "$LOCAL_LIBS" "$REMOTE_LIB_DIR"
-push_if_not_exists "$LOCAL_MODELS" "$REMOTE_MODELS_DIR"
+push_if_not_exists "$LOCAL_TINY_DIR" "$REMOTE_TINY_DIR"
+push_if_not_exists "$LOCAL_BASE_DIR" "$REMOTE_BASE_DIR"
+push_if_not_exists "$LOCAL_SMALL_DIR" "$REMOTE_SMALL_DIR"
 push_if_not_exists "$LOCAL_INPUTS" "$REMOTE_INPUTS_DIR"
