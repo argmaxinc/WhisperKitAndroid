1fd862c65fb3af60498a5d4b578abed89e7d396c
Keith Ha <keith@argmaxinc.com>
2025-01-01 13:07:56 -0800

    adding linux host test scripts

---
+++ test/linux_test_utils.py
@@ -0,0 +1,166 @@
+#
+# For licensing see accompanying LICENSE.md file.
+# Copyright (C) 2025 Argmax, Inc. All Rights Reserved.
+#
+
+import unittest
+import shutil
+import os
+import time
+import json
+import evaluate
+import docker
+from whisper.normalizers import EnglishTextNormalizer
+
+
+class TestRunLinux:
+    output_file = "output.json"
+    container_name = "axie_tflite"
+    docker_root = "/src/AXIE"
+    
+    def __init__(
+        self,
+        tokenizer
+    ):
+        self.root = os.path.dirname(os.path.abspath(__file__)) + "/.."
+        self.bin_path = self.docker_root + "/build/linux"
+        self.lib_path = self.docker_root + "/external/libs/linux"
+        self.output_json = None
+        self.tokenizer = tokenizer
+        self.text_normalizer = EnglishTextNormalizer()
+        self.docker = docker.from_env()
+        self.container = self.docker.containers.get(self.container_name)
+
+    def _run_docker_cmd(self, cmd):
+        cmds = f"/bin/bash -c '{cmd}'"
+        try:
+            _, result = self.container.exec_run(cmds, stream=True)
+        except Exception as e:
+            print(f"Error running a command on Docker..")
+            print(f"Is {self.container_name} running? Run 'make env' to start Docker container")
+            os._exit(-1)
+        
+        for log in result:
+            line = log.decode()
+            print(line)
+            find_duration = "Total Duration:"
+            pos = line.find(find_duration)
+            if pos > 0:
+                strs = line[(pos + len(find_duration) + 1): -1].split()
+                return float(strs[0])
+        return 0.0
+
+    def copy_file(self, file, subfolder='.'):
+        dest_folder =  f"{self.root}/{subfolder}/"
+        if not os.path.exists(dest_folder):
+            os.makedirs(dest_folder)
+        shutil.copy(file, dest_folder)
+
+    def device_test(self, test_bin, input_audio, model_size):
+        if test_bin is None: 
+            return False
+
+        test_cmds = " ".join(
+            [
+                f"export LD_LIBRARY_PATH={self.lib_path} &&",
+                f"{self.bin_path}/{test_bin} ",
+                f"{input_audio} {model_size} debug",
+            ]
+        )
+        print(f"Running: {test_cmds}")
+        duration = self._run_docker_cmd(test_cmds)
+        if duration <= 0.0:
+            print(f"An Error Occurred..")
+            return False
+        return True
+
+    def _get_wer(self, ref, pred):
+        wer_metric = evaluate.load("wer")
+        avg_wer = wer_metric.compute(
+            references=[ref,],
+            predictions=[pred,],
+        )
+        avg_wer = round(100 * avg_wer, 2)
+        return avg_wer
+
+    def _put_test_info(self, data_set, file, metadata):
+        reference = None
+        name_only, _ = os.path.splitext(file)
+        for data in metadata:
+            if str(data['audio']).find(name_only) != -1:
+                reference = data['text']
+                break
+
+        self.output_json["testInfo"]["datasetDir"] = data_set
+        self.output_json["testInfo"]["reference"] = reference
+        
+        prediction_token = self.output_json["testInfo"]["prediction"]
+        prediction_text = self.tokenizer.decode(prediction_token)
+        normalized = self.text_normalizer(prediction_text)
+        self.output_json["testInfo"]["prediction"] = normalized
+        self.output_json["testInfo"]["wer"] = self._get_wer(reference, normalized)
+
+    def run_test(
+            self, test_binary, 
+            file, data_set, 
+            metadata, model_size):
+        full_path = f"{self.docker_root}/inputs/{file}"
+        result = self.device_test(test_binary, full_path, model_size)
+        if result is False:
+            return None
+        
+        output_file = f"{self.root}/{TestRunLinux.output_file}"
+        if os.path.exists(output_file) is False:
+            return None
+
+        f = open(output_file)
+        self.output_json = json.load(f)
+        f.close()
+        os.remove(output_file)
+
+        self._put_test_info(data_set, file, metadata)
+        return self.output_json
+
+
+class LinuxTestsMixin(unittest.TestCase):
+    """ Mixin class for Linux test with audio input file
+    """
+    audio_file_ext = [".mp3", ".m4a", ".ogg", ".flac", ".aac", ".wav"]
+
+    @classmethod
+    def setUpClass(self):
+        self.test_no = 0
+
+    def run_test(self):
+        self.test_no += 1
+
+        if self.args.model_path.find("openai_whisper-tiny") != -1:
+            model_size = "tiny"
+        elif self.args.model_path.find("openai_whisper-base") != -1:
+            model_size = "base"
+        elif self.args.model_path.find("openai_whisper-small") != -1:
+            model_size = "small"
+
+        host = TestRunLinux(self.tokenizer)
+    
+        outputs_json = []
+        for file in self.files:
+            test_no = self.test_no
+            full_path = os.path.join(self.path, file)
+            host.copy_file(full_path, "inputs")
+
+            print(f'======== Running test #{test_no} (audio: {file}) on linux host ========')
+            
+            output = host.run_test(
+                self.test_bin, file, 
+                self.data_set, self.metadata, 
+                model_size)
+            
+            print(f'======== Completed test #{test_no} (audio: {file}) on linux host ========')
+
+            self.test_no += 1
+            outputs_json.append(output)
+            time.sleep(1)
+
+        json.dumps(outputs_json)
+        return outputs_json
--- test/whisperkit_test.py
+++ test/whisperkit_test.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from huggingface_hub import hf_hub_download
 from whisper.tokenizer import get_tokenizer
 from android_test_utils import AndroidTestsMixin
+from linux_test_utils import LinuxTestsMixin
+
 
 class TestWhisperKitAndroid(AndroidTestsMixin):
     TEST_DATASET = "librispeech-10mins"
@@ -106,6 +108,63 @@ class TestWhisperKitAndroid(AndroidTestsMixin):
                         json.dump(output_json, json_file)
 
 
+class TestWhisperKitLinux(LinuxTestsMixin):
+    @classmethod
+    def setUpClass(self):
+        super().setUpClass()
+
+    def __init__(self, methodName='runTest', args=None):
+        super().__init__(methodName)
+        self.args = args
+        self.test_bin = "whisperax_cli"
+        self.tokenizer = get_tokenizer(multilingual=False, language="en")
+
+        splitted = os.path.split(os.path.abspath(self.args.input))
+        path_parts = Path(self.args.input).parts
+        if os.path.isfile(self.args.input):
+            self.path = splitted[0]
+            self.files = [splitted[1]]
+            self.data_set = path_parts[-2]
+        
+        elif os.path.isdir(self.args.input):
+            self.path = self.args.input
+            self.data_set = path_parts[-1]
+            self.files = []
+
+        metadata_path = os.path.join(self.path, "metadata.json")
+        metadata_file = open(metadata_path)
+        self.metadata = json.load(metadata_file)
+        metadata_text = json.dumps(self.metadata)
+        metadata_file.close()
+
+        if os.path.isdir(self.args.input):
+            for file in os.listdir(self.args.input):
+                full_file_name = os.path.join(self.args.input, file)
+                # checking if it is a file
+                if os.path.isfile(full_file_name) is False:
+                    continue
+                
+                name_only, extension = os.path.splitext(file)
+                if extension not in self.audio_file_ext:
+                    continue
+
+                if metadata_text.find(name_only) == -1:
+                    continue
+
+                self.files.append(file)
+
+    def test_audio_transcription(self):
+        os.makedirs(self.args.output, exist_ok=True)
+
+        output_json = self.run_test()
+        if output_json is None:
+            print('test on host has failed..')
+            self.assertTrue(False)
+        with open(f"{self.args.output}/linux_report.json", "w")\
+            as json_file:
+            json.dump(output_json, json_file)
+
+
 def download_hg_dataset():
     test_path = f"{os.path.dirname(os.path.abspath(__file__))}/../test/"
     prefix = f"{test_path}/{TestWhisperKitAndroid.TEST_DATASET}"
@@ -167,7 +226,10 @@ if __name__ == "__main__":
             help='input file, folder, or (default) download from HuggingFace')
     parser.add_argument("-o", "--output",
             default="./output", type=str,
-            help='output json folder')    
+            help='output json folder')
+    parser.add_argument("-l", "--linux", 
+            action='store_true', 
+            help='linux or (default) android device(s)')
     args = parser.parse_args()
 
     if args.input is None:
@@ -177,8 +239,14 @@ if __name__ == "__main__":
     suite = unittest.TestSuite()
     runner = unittest.TextTestRunner()
 
-    test_names = test_loader.getTestCaseNames(TestWhisperKitAndroid)
-    for test_name in test_names:
-        suite.addTest(TestWhisperKitAndroid(test_name, args=args))
+    if args.linux:
+        test_names = test_loader.getTestCaseNames(TestWhisperKitLinux)
+        for test_name in test_names:
+            suite.addTest(TestWhisperKitLinux(test_name, args=args))
+    else:
+        test_names = test_loader.getTestCaseNames(TestWhisperKitAndroid)
+        for test_name in test_names:
+            suite.addTest(TestWhisperKitAndroid(test_name, args=args))
+
     allsuites = unittest.TestSuite([suite])
     runner.run(allsuites)
