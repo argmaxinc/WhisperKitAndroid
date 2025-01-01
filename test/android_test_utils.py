ed789519f299eeebc9deb333108122e80f4c6be3
Keith Ha <keith4ever@gmail.com>
2025-01-01 14:53:01 -0500

    adding android test (#21)
    * adding android test
    
    * adding python package requirements
    
    * updated per review
    
    * minimized requirements.txt
    
    * adding an example to help print
    
    * adding -o output path
    
    ---------
    
    Co-authored-by: Keith Ha <keith@argmaxinc.com>

---
+++ test/android_test_utils.py
@@ -0,0 +1,263 @@
+#
+# For licensing see accompanying LICENSE.md file.
+# Copyright (C) 2024 Argmax, Inc. All Rights Reserved.
+#
+
+import unittest
+import subprocess
+import os
+import time
+import json
+import evaluate
+from threading import Thread, Condition
+from whisper.normalizers import EnglishTextNormalizer
+
+
+class TestRunADB:
+    output_file = "output.json"
+
+    def __init__(
+        self,
+        root_path,
+        tokenizer,
+        serial
+    ):
+        """
+        Args:
+            bin_path, lib_path:
+                Path under /data/local/tmp for executorch binary & library files
+            root_path:   Path under /sdcard/argmax, for other audio/model files
+            serial:      device serial no.
+        """
+        self.bin_path = "/data/local/tmp/bin"
+        self.lib_path = "/data/local/tmp/lib"
+        self.root_path = root_path
+        self.executing = False
+        self.condition = Condition()
+        self.batts = []
+        self.temps = []
+        self.mem = 0
+        self.curr_cmd = None
+        self.output_json = None
+        self.probe_thread = None
+        self.tokenizer = tokenizer
+        self.text_normalizer = EnglishTextNormalizer()
+        self.serial = serial
+
+    def _adb(self, cmd):
+        cmds = ["adb", "-s", self.serial]
+        cmds.extend(cmd)
+        # subprocess.run(cmds, stdout=sys.stdout)
+        output = subprocess.run(cmds, stdout=subprocess.PIPE)
+        if output.stdout is None:
+            return None
+        return output.stdout.decode('utf-8')
+
+    def _check_device(self):
+        result = subprocess.run(["adb", "devices"], stdout=subprocess.PIPE)
+        output = result.stdout.decode()
+        if "device" in output:
+            if self.serial in output:
+                return True
+ 
+        print(f"ADB Device {self.serial} not found")
+        return False
+
+    def push_file(self, file, subfolder='.'):
+        dest_folder = f"{self.root_path}/{subfolder}/"
+        _ = self._adb(["push", file, dest_folder])
+
+    def device_test(self, test_bin, input_audio, model_size):
+        if self._check_device() is False:
+            return 
+        if test_bin is None: 
+            return 
+
+        input_audio = f"{self.root_path}/inputs/{input_audio}"
+        self.curr_cmd = test_bin
+        test_cmds = " ".join(
+            [
+                f"cd {self.root_path} &&",
+                f"export LD_LIBRARY_PATH={self.lib_path} &&",
+                f"{self.bin_path}/{self.curr_cmd} ",
+                f"{input_audio} {model_size} debug",
+            ]
+        )
+        print(f"Running: {test_cmds}")
+        self.executing = True
+        with self.condition:
+            self.condition.notify()
+
+        self.run_shell_cmds(f"{test_cmds}")
+        self.executing = False
+
+    def pull(self, file, callback=None):
+        device_path = f"{self.root_path}/{file}"
+        host_path = f"{self.serial}_{file}"
+        _ = self._adb(["pull", device_path, host_path])
+        if callback:
+            callback()
+        return host_path
+
+    def run_shell_cmds(self, command):
+        return self._adb(["shell", command])
+
+    def get_meminfo(self, test_binary):
+        strout = self.run_shell_cmds(f'dumpsys meminfo {test_binary}')
+        for line in strout.split('\n'):
+            if 'TOTAL RSS:' in line:
+                items = line.split()
+                if int(items[5]) > self.mem:
+                    self.mem = int(items[5])
+                break
+        return self.mem
+
+    def get_batterylevel(self):
+        strout = self.run_shell_cmds('dumpsys battery | grep level')
+        items = strout.split()
+
+        idx = 0
+        try:
+            idx = items.index("level:")
+        except:
+            return -1
+        
+        return int(items[idx+1])
+
+    def get_thermalinfo(self):
+        highest_temp = 0.0
+        strout = self.run_shell_cmds('dumpsys thermalservice')
+        for line in strout.split('\n'):
+            if 'mName=AP' in line or 'mName=CPU' in line:
+                substr = line.split('=')
+                substr = substr[1].split(',')
+                temp = float(substr[0])
+                if highest_temp < temp:
+                    highest_temp = temp
+        return highest_temp
+
+    def _probe_device(self, test_binary):
+        self.batts.append(self.get_batterylevel())
+        self.temps.append(self.get_thermalinfo())
+
+        if self.executing is False:
+            with self.condition:
+                self.condition.wait()
+
+        while self.executing:
+            self.get_meminfo(test_binary)
+            time.sleep(0.2)
+
+        self.batts.append(self.get_batterylevel())
+        self.temps.append(self.get_thermalinfo())
+
+    def _start_probe(self, test_binary):
+        self.probe_thread = Thread(
+                            target=self._probe_device, 
+                            args=(test_binary,))
+        self.probe_thread.start()
+
+    def _end_probe(self):
+        if self.probe_thread is None:
+            return 
+        self.probe_thread.join()
+
+    def _get_wer(self, ref, pred):
+        wer_metric = evaluate.load("wer")
+        avg_wer = wer_metric.compute(
+            references=[ref,],
+            predictions=[pred,],
+        )
+        avg_wer = round(100 * avg_wer, 2)
+        return avg_wer
+
+    def _put_test_info(self, data_set, file, metadata):
+        reference = None
+        name_only, _ = os.path.splitext(file)
+        for data in metadata:
+            if str(data['audio']).find(name_only) != -1:
+                reference = data['text']
+                break
+
+        device_info = {}
+        device_info["peak_mem"] = self.mem
+        device_info["cpu_temp"] = self.temps[1]
+        device_info["batt_level"] = self.batts[1]
+
+        self.output_json["deviceInfo"] = json.dumps(device_info)
+        self.output_json["testInfo"]["datasetDir"] = data_set
+        self.output_json["testInfo"]["reference"] = reference
+        
+        prediction_token = self.output_json["testInfo"]["prediction"]
+        prediction_text = self.tokenizer.decode(prediction_token)
+        normalized = self.text_normalizer(prediction_text)
+        self.output_json["testInfo"]["prediction"] = normalized
+        self.output_json["testInfo"]["wer"] = self._get_wer(reference, normalized)
+
+    def run_test(
+            self, test_binary, 
+            file, data_set, 
+            metadata, model_size):
+        self._start_probe(test_binary)
+        result = self.device_test(test_binary, file, model_size)
+        self._end_probe()
+
+        if result is False:
+            return None
+        
+        output_file = self.pull(file=TestRunADB.output_file)
+
+        if os.path.exists(output_file) is False:
+            return None
+
+        f = open(output_file)
+        self.output_json = json.load(f)
+        f.close()
+        os.remove(output_file)
+
+        self._put_test_info(data_set, file, metadata)
+        return self.output_json
+
+
+class AndroidTestsMixin(unittest.TestCase):
+    """ Mixin class for Android device test with audio input file
+    """
+    audio_file_ext = [".mp3", ".m4a", ".ogg", ".flac", ".aac", ".wav"]
+
+    @classmethod
+    def setUpClass(self):
+        self.test_no = 0
+
+    def run_test(self, device):
+        self.test_no += 1
+
+        if self.args.model_path.find("openai_whisper-tiny") != -1:
+            model_size = "tiny"
+        elif self.args.model_path.find("openai_whisper-base") != -1:
+            model_size = "base"
+        elif self.args.model_path.find("openai_whisper-small") != -1:
+            model_size = "small"
+
+        adb = TestRunADB(self.root_path, self.tokenizer, device)
+    
+        outputs_json = []
+        for file in self.files:
+            test_no = self.test_no
+            full_path = os.path.join(self.path, file)
+            adb.push_file(full_path, "inputs")
+
+            print(f'======== Running test #{test_no} (audio: {file}) on {device} ========')
+            
+            output = adb.run_test(
+                self.test_bin, 
+                file, self.data_set, 
+                self.metadata, model_size)
+            
+            print(f'======== Completed test #{test_no} (audio: {file}) on {device} ========')
+
+            self.test_no += 1
+            outputs_json.append(output)
+            time.sleep(1)
+
+        json.dumps(outputs_json)
+        return outputs_json
---
+++ test/whisperkit_test.py
@@ -0,0 +1,184 @@
+#
+# For licensing see accompanying LICENSE.md file.
+# Copyright (C) 2023 Argmax, Inc. All Rights Reserved.
+#
+
+import os
+import json
+import unittest
+import argparse
+import subprocess
+import concurrent.futures as futures
+
+from pathlib import Path
+from huggingface_hub import hf_hub_download
+from whisper.tokenizer import get_tokenizer
+from android_test_utils import AndroidTestsMixin
+
+class TestWhisperKitAndroid(AndroidTestsMixin):
+    TEST_DATASET = "librispeech-10mins"
+
+    @classmethod
+    def setUpClass(self):
+        super().setUpClass()
+
+    def __init__(self, methodName='runTest', args=None):
+        super().__init__(methodName)
+        self.args = args
+        self.test_bin = "whisperax_cli"
+        self.root_path = "/sdcard/argmax/tflite"
+        self.devices = []
+        self.tokenizer = get_tokenizer(multilingual=False, language="en")
+
+        output = subprocess.run(["adb", "devices"], stdout=subprocess.PIPE)
+        if output.stdout is None:
+            return None
+        devices = output.stdout.decode('utf-8')
+        words = devices.split()
+
+        attached_found = False
+        for index, word in enumerate(words):
+            if attached_found is False:
+                if word.find('attached') != -1:
+                    attached_found = True
+                else:
+                    continue
+
+            if len(words) > index + 1:
+                if words[index + 1].find("device") != -1:
+                    self.devices.append(word)
+
+        splitted = os.path.split(os.path.abspath(self.args.input))
+        path_parts = Path(self.args.input).parts
+        if os.path.isfile(self.args.input):
+            self.path = splitted[0]
+            self.files = [splitted[1]]
+            self.data_set = path_parts[-2]
+        
+        elif os.path.isdir(self.args.input):
+            self.path = self.args.input
+            self.data_set = path_parts[-1]
+            self.files = []
+
+        metadata_path = os.path.join(self.path, "metadata.json")
+        metadata_file = open(metadata_path)
+        self.metadata = json.load(metadata_file)
+        metadata_text = json.dumps(self.metadata)
+        metadata_file.close()
+
+        if os.path.isdir(self.args.input):
+            for file in os.listdir(self.args.input):
+                full_file_name = os.path.join(self.args.input, file)
+                # checking if it is a file
+                if os.path.isfile(full_file_name) is False:
+                    continue
+                
+                name_only, extension = os.path.splitext(file)
+                if extension not in self.audio_file_ext:
+                    continue
+
+                if metadata_text.find(name_only) == -1:
+                    continue
+
+                self.files.append(file)
+
+    def test_audio_transcription(self):
+        os.makedirs(self.args.output, exist_ok=True)
+            
+        with futures.ThreadPoolExecutor(max_workers=len(self.devices)) as executor:
+            future_test = { 
+                executor.submit(self.run_test, device):
+                    device for device in self.devices
+            }
+
+            for future in futures.as_completed(future_test):
+                device = future_test[future]
+                try: 
+                    output_json = future.result()
+                except Exception as exc:
+                    print('device %r generated an exception: %s' % (device, exc))
+                else:
+                    if output_json is None:
+                        print('device %r test has failed..')
+                        self.assertTrue(False)
+                    with open(f"{self.args.output}/{device}_report.json", "w")\
+                        as json_file:
+                        json.dump(output_json, json_file)
+
+
+def download_hg_dataset():
+    test_path = f"{os.path.dirname(os.path.abspath(__file__))}/../test/"
+    prefix = f"{test_path}/{TestWhisperKitAndroid.TEST_DATASET}"
+    if os.path.exists(f"{prefix}/metadata.json"):
+        return prefix
+    
+    hf_hub_download(
+                "argmaxinc/whisperkit-test-data",
+                filename="metadata.json",
+                subfolder=TestWhisperKitAndroid.TEST_DATASET,
+                local_dir=test_path,
+                repo_type="dataset",
+                revision="main",
+    )
+    metadata_file = open(os.path.join(prefix, "metadata.json"))
+    metadata = json.load(metadata_file)
+    metadata_file.close()
+    
+    file_name_filter = ["61-", "121-", "4507-"]
+    for item in metadata:
+        if "audio" not in item:
+            continue
+        if not any([item["audio"].startswith(f) for f in file_name_filter]):
+            continue
+
+        file = item["audio"].split(".")[0] + ".mp3"
+        if os.path.exists(f"{prefix}/{file}"):
+            continue
+        try:
+            hf_hub_download(
+                "argmaxinc/whisperkit-test-data",
+                filename=file,
+                subfolder=TestWhisperKitAndroid.TEST_DATASET,
+                local_dir=test_path,
+                repo_type="dataset",
+                revision="main",
+            )
+            print(f'downloaded {file}')
+        except:
+            print(' ') # this is not an issue, do nothing
+        
+    return prefix
+
+
+class ArgParser(argparse.ArgumentParser):
+    def print_help(self, file = None):
+        super().print_help(file)
+        print("example: ")
+        print("  python3.10 test/whisperkit_test.py -m ./openai_whisper-tiny")
+
+
+if __name__ == "__main__":
+    parser = ArgParser()
+    parser.add_argument("-m", "--model-path", 
+            default="openai_whisper-tiny", type=str,
+            help='path to the models, e.g., openai_whisper-tiny')
+    parser.add_argument("-i", "--input",
+            default=None, type=str,
+            help='input file, folder, or (default) download from HuggingFace')
+    parser.add_argument("-o", "--output",
+            default="./output", type=str,
+            help='output json folder')    
+    args = parser.parse_args()
+
+    if args.input is None:
+        args.input = download_hg_dataset()
+
+    test_loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    runner = unittest.TextTestRunner()
+
+    test_names = test_loader.getTestCaseNames(TestWhisperKitAndroid)
+    for test_name in test_names:
+        suite.addTest(TestWhisperKitAndroid(test_name, args=args))
+    allsuites = unittest.TestSuite([suite])
+    runner.run(allsuites)
