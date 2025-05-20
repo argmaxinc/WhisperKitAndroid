b91ffb5f66fd057901c5a745c12cbf8048a47372
ZachNagengast <znagengast@gmail.com>
2025-05-20 12:00:00 -0700

    Optimize imports

--- android/examples/WhisperAX/src/main/java/com/argmaxinc/whisperax/MainActivity.kt
+++ android/examples/WhisperAX/src/main/java/com/argmaxinc/whisperax/MainActivity.kt
@@ -2,14 +2,9 @@
 //  Copyright © 2025 Argmax, Inc. All rights reserved.
 package com.argmaxinc.whisperax
 
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -21,7 +16,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
-import android.Manifest
 
 class MainActivity : ComponentActivity() {
     companion object {
